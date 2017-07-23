0a049235b52ed7dc868ca737ebfecab5154ecb45
Alex D. Zharichenko <alex.zharichenko@quantumcode.io>
2017-07-23 00:44:18 -0400

    Add extraction of catalog subjects

--- PittWebAPI/scraper/subjects.py
+++ PittWebAPI/scraper/subjects.py
@@ -1,27 +1,43 @@
-import string
-from pprint import pprint
-
+import requests
 import grequests
 from bs4 import BeautifulSoup
 
+LETTERS = 'ABCDEFGHIJLMNOPRSTUW'
+TO_BE_DETERMINED = 'KQV'  # TODO(Alex Z.) Look into ... to help assist with this
 
-ALPHA = string.ascii_uppercase
+CATALOG_URL = 'https://psmobile.pitt.edu/app/catalog/listCatalog'
 BASE_URL = 'https://psmobile.pitt.edu/app/catalog/listSubjectsByLetter/UPITT/'
 
 
+# TODO(Alex Z.) Be able to fetch direct from course catalog
+# def _fetch_catalog_letters():
+#     response = requests.get(CATALOG_URL)
+#     soup = BeautifulSoup(response.text, 'lxml')
+#     return soup
+
+
 def _fetch_all_departments():
-    urls = [grequests.get(BASE_URL + char) for char in ALPHA]
+    urls = [grequests.get(BASE_URL + char) for char in LETTERS]
     return grequests.map(urls)
 
 
+def _extract_subjects(tags):
+    subjects = [
+        tuple(tag.text.split(' - '))
+        for tag in tags
+    ]
+    return subjects
+
+
 def _parse_subjects(responses):
+    subject_catalog = []
     for response in responses:
         soup = BeautifulSoup(response.text, 'lxml')
         parent = soup.find('div', {'class', 'primary-head'}).parent
         subjects = parent.findAll('div', {'class': 'strong section-body'})
-    pass
+        subject_catalog.append(_extract_subjects(subjects))
+    return subject_catalog
 
 
 responses = _fetch_all_departments()
-pprint(responses)
-# subject_codes = _parse_subjects(responses)
+subject_codes = _parse_subjects(responses)
